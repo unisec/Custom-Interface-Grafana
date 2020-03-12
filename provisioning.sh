dce9a87b0ec31c6a7a2e421450b37a5197f2b5f6
jadson179 <jadson44.santos@gmail.com>
2020-03-12 16:55:23 -0300

    Arquivo de provisionamento finalizado

--- provisioning.sh
+++ provisioning.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
+echo "Instalando curl"
 
-echo "Acessando diretório /usr/share/grafana/public/img " 
-
-cd /usr/share/grafana/public/img
+apk add curl 
 
 echo "Reescrevendo imagem heatmap_bg_test.svg"
 
-curl https://raw.githubusercontent.com/unisec/Custom-Interface-Grafana/develop/imgs/heatmap_bg_test.svg 
+curl https://raw.githubusercontent.com/unisec/Custom-Interface-Grafana/develop/imgs/heatmap_bg_test.svg --output /usr/share/grafana/public/img/heatmap_bg_test.svg
 
 echo "Reescrevendo imagem fav32.png"
 
-curl https://raw.githubusercontent.com/unisec/Custom-Interface-Grafana/develop/imgs/fav32.png
+curl https://raw.githubusercontent.com/unisec/Custom-Interface-Grafana/develop/imgs/fav32.png --output /usr/share/grafana/public/img/fav32.png
 
 echo "Reescrevendo imagem grafana_icon.svg" 
 
-curl https://raw.githubusercontent.com/unisec/Custom-Interface-Grafana/develop/imgs/grafana_icon.svg
+curl https://raw.githubusercontent.com/unisec/Custom-Interface-Grafana/develop/imgs/grafana_icon.svg --output /usr/share/grafana/public/img/grafana_icon.svg
 
 echo "Reescrevendo imagem grafana_typelogo.svg" 
 
-curl https://raw.githubusercontent.com/unisec/Custom-Interface-Grafana/develop/imgs/grafana_typelogo.svg
+curl https://raw.githubusercontent.com/unisec/Custom-Interface-Grafana/develop/imgs/grafana_typelogo.svg --output /usr/share/grafana/public/img/grafana_typelogo.svg
 
 cd /usr/share/grafana/public/views
 
-curl https://raw.githubusercontent.com/unisec/Custom-Interface-Grafana/develop/views/index.html
+curl https://raw.githubusercontent.com/unisec/Custom-Interface-Grafana/develop/views/index.html --output /usr/share/grafana/public/views/index.html
